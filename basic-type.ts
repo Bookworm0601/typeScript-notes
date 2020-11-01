914107c045cbdc312ae377f223820606e8e705ee
Adair <761582196@qq.com>
2020-11-01 21:11:49 +0800

    基础数据类型

---
+++ basic-type.ts
@@ -0,0 +1,10 @@
+// boolen类型 小写
+let isShow: boolean = true
+let age: number = 12
+let binInt: number = 0b111111
+let firstName: string = 'gao'
+let info: string = `my name is ${firstName}, age is ${age}`
+let u: undefined = undefined
+let n: null = null
+// u n是所有类型的子集
+let show: boolean = undefined
