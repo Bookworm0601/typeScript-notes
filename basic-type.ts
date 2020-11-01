0ca1ec81fd6d60c753d3eb8921c3aebbf1685ae9
Adair <761582196@qq.com>
2020-11-01 21:23:12 +0800

    any类型和联合类型

--- basic-type.ts
+++ basic-type.ts
@@ -8,3 +8,15 @@ let u: undefined = undefined
 let n: null = null
 // u n是所有类型的子集
 let show: boolean = undefined
+
+
+// any类型 可以兼容任何类型(不安全，无属性方法提示)
+
+let anyData: any = 'xxxx'
+anyData = 123
+anyData.getName()
+
+
+//联合类型
+let stringOrNumber: string | number = 1
+stringOrNumber = '123'
