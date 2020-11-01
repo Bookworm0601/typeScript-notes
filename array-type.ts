c123b4bbccae7bab6d4430223395d40c375b8638
Adair <761582196@qq.com>
2020-11-01 22:46:04 +0800

    interface相关内容

--- array-type.ts
+++ array-type.ts
@@ -1,7 +1,7 @@
 // 数组中只能有字符串元素
 let strArr:string[] = ['aaa']
 // 元组(严格匹配)
-let unitArr:[string,number] = ['111', 3]
+let unitArr:[string | number,number] = [2, 3]
 
 function likeArr() {
  // arguments是类数组，有length和下标获取参数，但是无数组方法   
